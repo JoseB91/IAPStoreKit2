6dd63f9f9e15a86c5743af1cce502fb241786d3c
JoseB91 <jose.briones.r@hotmail.com>
2025-05-13 16:13:07 -0500

    Create project

---
+++ IAPStoreKit2/IAPStoreKit2/IAPStoreKit2App.swift
@@ -0,0 +1,17 @@
+//
+//  IAPStoreKit2App.swift
+//  IAPStoreKit2
+//
+//  Created by José Briones on 13/5/25.
+//
+
+import SwiftUI
+
+@main
+struct IAPStoreKit2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
